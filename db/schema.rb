1fcd56f7f0bff07efe8c65f8d881455b6dba92c3
Alethia Quintero <alethiaq22@gmail.com>
2024-10-09 11:46:36 -0400

    add level column to character table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_07_150316) do
+ActiveRecord::Schema.define(version: 2024_10_09_154551) do
 
   create_table "characters", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2024_10_07_150316) do
     t.text "metadata"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "level"
   end
 
 end
