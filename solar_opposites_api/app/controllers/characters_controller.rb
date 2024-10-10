cd3331cc528f7523aef1d2af3bc627560366d8f0
Alethia Quintero <alethiaq22@gmail.com>
2024-10-10 11:49:26 -0400

    render json instead of rails view html page

--- solar_opposites_api/app/controllers/characters_controller.rb
+++ solar_opposites_api/app/controllers/characters_controller.rb
@@ -2,9 +2,11 @@ class CharactersController < ApplicationController
 
   def index 
     @characters = Character.all
+    render json: @characters
   end
 
   def show 
     @character = Character.find(params[:id])
+    render json: @character
   end
 end
